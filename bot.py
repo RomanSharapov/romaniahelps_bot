0ee6b68d9bd1745a3fa0d93feaa05e6f72a226dc
Roman Sharapov <info@romansharapov.com>
2022-02-25 23:49:28 -0500

    Workflow updated

--- bot.py
+++ bot.py
@@ -26,92 +26,64 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-GENDER, PHOTO, LOCATION, BIO = range(4)
+HELP_NEEDED, LOCATION, CONTACTS = range(3)
 
 
 def start(update: Update, context: CallbackContext) -> int:
-    """Starts the conversation and asks the user about their gender."""
-    reply_keyboard = [['Boy', 'Girl', 'Other']]
+    """Starts the conversation and asks the user about help message."""
 
     update.message.reply_text(
-        'Hi! My name is Professor Bot. I will hold a conversation with you. '
-        'Send /cancel to stop talking to me.\n\n'
-        'Are you a boy or a girl?',
-        reply_markup=ReplyKeyboardMarkup(
-            reply_keyboard, one_time_keyboard=True, input_field_placeholder='Boy or Girl?'
-        ),
+        'Hi! Romanians Help Bot will help you to connect with volunteers in Romania. '
+        'Send /cancel to stop interaction.\n\n'
+        'What kind of help do you need (e.g. accomodation, food, or something else)?'
     )
 
-    return GENDER
+    return HELP_NEEDED
 
 
-def gender(update: Update, context: CallbackContext) -> int:
+def help_needed(update: Update, context: CallbackContext) -> int:
     """Stores the selected gender and asks for a photo."""
     user = update.message.from_user
-    logger.info("Gender of %s: %s", user.first_name, update.message.text)
+    logger.info("User %s needs help with: %s", user.first_name, update.message.text)
     update.message.reply_text(
-        'I see! Please send me a photo of yourself, '
-        'so I know what you look like, or send /skip if you don\'t want to.',
+        'I see! Please send me your location, '
+        'so volunteers can find you, or send /skip if you don\'t want to.',
         reply_markup=ReplyKeyboardRemove(),
-    )
-
-    return PHOTO
-
-
-def photo(update: Update, context: CallbackContext) -> int:
-    """Stores the photo and asks for a location."""
-    user = update.message.from_user
-    photo_file = update.message.photo[-1].get_file()
-    photo_file.download('user_photo.jpg')
-    logger.info("Photo of %s: %s", user.first_name, 'user_photo.jpg')
-    update.message.reply_text(
-        'Gorgeous! Now, send me your location please, or send /skip if you don\'t want to.'
-    )
-
-    return LOCATION
-
-
-def skip_photo(update: Update, context: CallbackContext) -> int:
-    """Skips the photo and asks for a location."""
-    user = update.message.from_user
-    logger.info("User %s did not send a photo.", user.first_name)
-    update.message.reply_text(
-        'I bet you look great! Now, send me your location please, or send /skip.'
     )
 
     return LOCATION
 
 
 def location(update: Update, context: CallbackContext) -> int:
-    """Stores the location and asks for some info about the user."""
+    """Stores the location and asks for some the user's contacts."""
     user = update.message.from_user
     user_location = update.message.location
     logger.info(
         "Location of %s: %f / %f", user.first_name, user_location.latitude, user_location.longitude
     )
     update.message.reply_text(
-        'Maybe I can visit you sometime! At last, tell me something about yourself.'
+        'Thank you! At last, tell me how our volunteers can contact you.'
     )
 
-    return BIO
+    return CONTACTS
 
 
 def skip_location(update: Update, context: CallbackContext) -> int:
-    """Skips the location and asks for info about the user."""
+    """Skips the location and asks for the user contacts."""
     user = update.message.from_user
     logger.info("User %s did not send a location.", user.first_name)
     update.message.reply_text(
         'You seem a bit paranoid! At last, tell me something about yourself.'
     )
 
-    return BIO
+    return CONTACTS
 
 
-def bio(update: Update, context: CallbackContext) -> int:
+def contacts(update: Update, context: CallbackContext) -> int:
     """Stores the info about the user and ends the conversation."""
     user = update.message.from_user
-    logger.info("Bio of %s: %s", user.first_name, update.message.text)
-    update.message.reply_text('Thank you! I hope we can talk again some day.')
+    logger.info("Contacts of %s: %s", user.first_name, update.message.text)
+    update.message.reply_text('Thank you! Romanian volunteers will reach out to you shortly.')
 
     return ConversationHandler.END
 
@@ -121,7 +93,7 @@ def cancel(update: Update, context: CallbackContext) -> int:
     user = update.message.from_user
     logger.info("User %s canceled the conversation.", user.first_name)
     update.message.reply_text(
-        'Bye! I hope we can talk again some day.', reply_markup=ReplyKeyboardRemove()
+        'Interaction canceled! Stay safe.', reply_markup=ReplyKeyboardRemove()
     )
 
     return ConversationHandler.END
@@ -133,30 +105,25 @@ def main() -> None:
     if BOT_TOKEN is None:
         raise EnvironmentError("Please, set BOT_TOKEN environment variable with the bot token from @BotFather")
 
-    # Create the Updater and pass it your bot's token.
     updater = Updater(BOT_TOKEN)
 
-    # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
 
-    # Add conversation handler with the states GENDER, PHOTO, LOCATION and BIO
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler(['start', 'starthelp'], start)],
         states={
-            GENDER: [MessageHandler(Filters.regex('^(Boy|Girl|Other)$'), gender)],
-            PHOTO: [MessageHandler(Filters.photo, photo), CommandHandler('skip', skip_photo)],
+            HELP_NEEDED: [MessageHandler(Filters.text & ~Filters.command, help_needed)],
             LOCATION: [
                 MessageHandler(Filters.location, location),
                 CommandHandler('skip', skip_location),
             ],
-            BIO: [MessageHandler(Filters.text & ~Filters.command, bio)],
+            CONTACTS: [MessageHandler(Filters.text & ~Filters.command, contacts)],
         },
         fallbacks=[CommandHandler('cancel', cancel)],
     )
 
     dispatcher.add_handler(conv_handler)
 
-    # Configure the bot's webhook
     updater.start_webhook(
         listen="0.0.0.0",
         port=PORT,
