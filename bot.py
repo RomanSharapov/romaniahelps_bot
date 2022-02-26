5412da958ea0ab9f0f570de6b5072ae5911b98dc
Roman Sharapov <info@romansharapov.com>
2022-02-26 00:41:58 -0500

    Gather user data

--- bot.py
+++ bot.py
@@ -35,7 +35,7 @@ def start(update: Update, context: CallbackContext) -> int:
     update.message.reply_text(
         'Hi! Romanians Help Bot will help you to connect with volunteers in Romania. '
         'Send /cancel to stop interaction.\n\n'
-        'What kind of help do you need (e.g. accomodation, food, or something else)?'
+        'What kind of help do you need (e.g. accomodation, food, or something else)?',
     )
 
     return HELP_NEEDED
@@ -44,11 +44,13 @@ def start(update: Update, context: CallbackContext) -> int:
 def help_needed(update: Update, context: CallbackContext) -> int:
     """Stores the selected gender and asks for a photo."""
     user = update.message.from_user
-    logger.info("User %s needs help with: %s", user.first_name, update.message.text)
+    text = update.message.text
+    context.user_data['help_needed'] = text
+
+    logger.info("User %s needs help with: %s", user.first_name, text)
     update.message.reply_text(
         'I see! Please send me your location, '
         'so volunteers can find you, or send /skip if you don\'t want to.',
-        reply_markup=ReplyKeyboardRemove(),
     )
 
     return LOCATION
@@ -58,6 +60,10 @@ def location(update: Update, context: CallbackContext) -> int:
     """Stores the location and asks for some the user's contacts."""
     user = update.message.from_user
     user_location = update.message.location
+
+    user_data = context.user_data
+    user_data['location'] = (user_location.latitude, user_location.longitude)
+
     logger.info(
         "Location of %s: %f / %f", user.first_name, user_location.latitude, user_location.longitude
     )
@@ -71,9 +77,12 @@ def location(update: Update, context: CallbackContext) -> int:
 def skip_location(update: Update, context: CallbackContext) -> int:
     """Skips the location and asks for the user contacts."""
     user = update.message.from_user
+    user_data = context.user_data
+    user_data['location'] = (None, None)
+
     logger.info("User %s did not send a location.", user.first_name)
     update.message.reply_text(
-        'You seem a bit paranoid! At last, tell me something about yourself.'
+        'That\'s fine, we respect your privacy! At last, tell me how our volunteers can contact you.'
     )
 
     return CONTACTS
@@ -82,8 +91,15 @@ def skip_location(update: Update, context: CallbackContext) -> int:
 def contacts(update: Update, context: CallbackContext) -> int:
     """Stores the info about the user and ends the conversation."""
     user = update.message.from_user
-    logger.info("Contacts of %s: %s", user.first_name, update.message.text)
+    user_data = context.user_data
+    text = update.message.text
+    context.user_data['contacts'] = text
+
+    logger.info("Contacts of %s: %s", user.first_name, text)
     update.message.reply_text('Thank you! Romanian volunteers will reach out to you shortly.')
+
+
+    logger.info("Gathered data: %s", user_data)
 
     return ConversationHandler.END
 
@@ -91,11 +107,14 @@ def contacts(update: Update, context: CallbackContext) -> int:
 def cancel(update: Update, context: CallbackContext) -> int:
     """Cancels and ends the conversation."""
     user = update.message.from_user
+    user_data = context.user_data
+
     logger.info("User %s canceled the conversation.", user.first_name)
     update.message.reply_text(
         'Interaction canceled! Stay safe.', reply_markup=ReplyKeyboardRemove()
     )
 
+    user_data.clear()
     return ConversationHandler.END
 
 
